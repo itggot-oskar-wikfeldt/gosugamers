5ecd99d9038435cfeffdbe45b226e90762b5fb4e
hsogge <oskarw99@gmail.com>
2016-12-06 03:17:58 +0100

    asd

--- player.rb
+++ player.rb
@@ -9,26 +9,36 @@ class Player
     @velY = 0
     @accelX = 0
     @accelY = 0
-    @max_speed = 20
-    @texture = Gosu::Image.new('./res/bunny.png')
+    @friction = 0.5
+    @max_speed = 10
+    @gravity = 1
+    @has_jumped = false
+    @on_ground = false
+    @left = Gosu::Image.new('./res/bunny_left.png')
+    @right = @texture = Gosu::Image.new('./res/bunny_right.png')
   end
   def fall
-    if @y+52+@velY < 480-48
-      @accelY = 2
+    if !@on_ground
+      @accelY = @gravity
     else
       @accelY = 0
       @velY = 0
+      @has_jumped = false
     end
-
   end
   def accelerate
-    @velX += @accelX if @velX.abs < @max_speed
-    @velY += @accelY if @velY.abs < @max_speed
+    if @velX.abs < @max_speed
+      @velX += @accelX
+    end
+    @velY += @accelY
   end
   def decellerate
-    if @accelX.abs > 0.5
-      @accelX > 0 ? @accelX -= 1 : @accelX += 1
+    if @velX.abs > 0.1
+      @velX > 0 ? @velX -= @friction : @velX += @friction
+    else
+      @velX = 0
     end
+
   end
   def move
     @x += @velX
@@ -36,16 +46,29 @@ class Player
   end
 
   def update
-    if (Gosu::button_down? Gosu::KbLeft) && (@accelX > -7)
-      @accelX -= 1
-    end
-    if (Gosu::button_down? Gosu::KbRight) && (@accelX < 7)
-      @accelX += 1
-    end
-    fall
+
+    @accelX = 0
     decellerate
+    if Gosu::button_down? Gosu::KbLeft
+      @accelX = -1
+      @texture = @left
+    end
+
+    if Gosu::button_down? Gosu::KbRight
+      @texture = @right
+      @accelX = 1
+    end
+    if (Gosu::button_down? Gosu::KbUp) && @has_jumped == false
+      @velY = -15
+      @has_jumped = true
+    end
+    @y+52+@velY < 480-48 ? @on_ground = false : @on_ground = true
+    fall
     accelerate
     move
+    p @has_jumped
+
+
 
 
 
